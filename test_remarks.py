1a0bd1ce9e566bb6cbe45f9bfefd11a34107f96b
franccesco <francc3sco@gmail.com>
2017-08-24 14:30:32 -0600

    better test

--- test_remarks.py
+++ test_remarks.py
@@ -1,19 +1,22 @@
 import unittest
-import random
+import modules
 import os
-from modules.file_operations import *
+import time
+import random
 
 class EntityTestCase(unittest.TestCase):
     """Test new_entity function."""
 
     def setUp(self):
-        """Create a random number to emulate an entity."""
-        self.random_number = str(random.randint(100000, 999999)) + '.md'
-        self.new_file_dir = path_structure + '/' + self.random_number
+        self.new_entity_id = str(random.randint(100000, 999999))
+        self.path = 'entities/' + time.strftime('%Y/%m/%d')
+        self.id_full_path = self.path + '/' + self.new_entity_id + '.md'
 
     def test_file_creation(self):
         """Test random id file and empty file."""
-        self.new_file = create_entity(self.random_number)
-        self.assertFalse(os.path.isdir(self.new_file_dir))
+        modules.io.create_entity(self.new_entity_id)
+        # check if file exist
+        self.assertTrue(os.path.isfile(self.id_full_path))
+        self.assertEqual(self.id_full_path, self.id_full_path)
 
 unittest.main()
