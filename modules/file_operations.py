26cb8287a4d5bbad286829de63e9e3a1f2fe1130
Franccesco Orozco <francc3sco@gmail.com>
2017-08-26 11:23:51 -0600

    Create different templates

--- modules/file_operations.py
+++ modules/file_operations.py
@@ -21,36 +21,37 @@ def create_dir():
         print("Directory '" + path_structure + "' does not exist. Creating...")
         os.makedirs(path_structure)
 
-def create_entity(new_entity = 'draft_entity', AM = False, PEP = False, overwrite = False):
+def create_entity(new_entity = 'draft_entity', entity_AM = False, entity_PEP = False, overwrite = False):
     """check if file exist. if not, then proceed to create."""
-
     empty_template = 'templates/empty_template.md'
     am_template = 'templates/am_template.md'
     am_pep_template = 'templates/am_pep_template.md'
-
+    pep_template = 'templates/pep_template.md'
     # Ask for new entity
     new_entity_estructure = path_structure + '/' + new_entity + '.md'
 
-    # checks if file exist, if false: create new entity.
-    if os.path.isfile(new_entity_estructure):
-        return False
-
-    elif AM == False and PEP == False:
+    if entity_AM == False and entity_PEP == False:
         # copy empty skeleton
         create_dir()
         copyfile(empty_template, new_entity_estructure)
         return True
 
-    elif AM == True and PEP == False:
+    elif entity_AM == True and entity_PEP == False:
         # copy adverse media only template
         create_dir()
         copyfile(am_template, new_entity_estructure)
         return True
 
-    elif AM == True and PEP == True:
-        # copy PEP template with adverse media
-        create_dir
+    elif entity_AM == True and entity_PEP == True:
+        # copy entity_PEP template with adverse media
+        create_dir()
         copyfile(am_pep_template, new_entity_estructure)
+        return True
+
+    elif entity_AM == False and entity_PEP == True:
+        # copy entity_PEP template with no adverse media
+        create_dir()
+        copyfile(pep_template, new_entity_estructure)
         return True
 
     else:
